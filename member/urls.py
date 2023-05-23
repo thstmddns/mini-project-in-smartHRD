91a4e7a2a855016774a24e8c551b85f6c1d22c7f
손승운 <thstmddns@naver.com>
2023-05-24 08:50:23 +0900

    2023.05.24첫번째

--- member/urls.py
+++ member/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from.import views
 
+app_name='member'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('member/', include('member.urls')),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('delete/', views.delete, name='delete'),
+    path('update/', views.update, name='update'),
+    path('password/', views.change_password, name='change_password'),
+    path('<username>/', views.profile, name = 'profile'),
 ]
